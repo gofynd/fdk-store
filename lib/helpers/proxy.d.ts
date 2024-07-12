352640dd9ed04e5c96587fd9b0604539f209d28e
Jigar Dafda <jigar.dafda@gmail.com>
2024-07-12 11:50:14 +0000

    [Auto Generated]

--- lib/helpers/proxy.d.ts
+++ lib/helpers/proxy.d.ts
@@ -1,2 +1,3 @@
+import type { ThunkActionDispatch } from "redux-thunk";
 import { EventEmitterFunction } from "../types";
-export declare function createCacheProxy<T extends object>(moduleName: string, instance: T, eventEmitter?: EventEmitterFunction): T;
+export declare function createCacheProxy<T extends object>(moduleName: string, instance: T, dispatch: ThunkActionDispatch<any>, eventEmitter?: EventEmitterFunction): T;
